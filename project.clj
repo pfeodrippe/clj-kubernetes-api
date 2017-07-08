1f5c7fc826c6fc58ffc6ce5d1b20cdd67921e4df
pfeodrippe <pfeodrippe@gmail.com>
2017-07-08 16:10:13 -0300

    update project name to not conflict

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject kubernetes-api "0.3.1-SNAPSHOT"
-  :description "Kubernetes Client API Library"
-  :url "https://github.com/yanatan16/clj-kubernetes-api"
+(defproject kubernetes-201707-api "0.3.2-SNAPSHOT"
+  :description "Kubernetes Client API Library - a fork fron yanatan16/clj-kubernetes-api with v1 swagger updated"
+  :url "https://github.com/pfeodrippe/clj-kubernetes-api"
   :license {:name "MIT"
-            :url "https://github.com/yanatan16/clj-kubernetes-api/blob/master/LICENSE"}
+            :url "https://github.com/pfeodrippe/clj-kubernetes-api/blob/master/LICENSE"}
 
   :plugins [[s3-wagon-private "1.3.0"]]
 
